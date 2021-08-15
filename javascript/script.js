bc53a2af02cfbd5387ccbe5c16dd0b9540397c86
Yuri Oliveira Marques <enviosdejogos@hotmail.com>
2021-08-15 19:58:05 -0300

    modal refatorado com classe

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from './scroll-suave.js';
 import Accordion from './accordion.js';
 import tabNav from './tabnav.js';
-import initModal from './modal.js';
+import Modal from './modal.js';
 import initTooltip from './tooltip.js';
 import initDropDownMenu from './dropdown-menu.js';
 import initiMenuMobile from './menu-mobile.js';
@@ -21,7 +21,9 @@ const tabNav = new tabnav('[data-tab="menu"] li', '[data-tab="content"] section'
 tabNav.init();
 
 
-initModal();
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
 initTooltip();
 initDropDownMenu();
 initiMenuMobile();
